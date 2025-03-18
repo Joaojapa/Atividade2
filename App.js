94585fe52f4580846f3c188b38e2b9473cf8378d
Joaojapa <jvitor@iesb.edu.br>
2025-03-18 15:10:50 -0300

    a

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -16,8 +16,11 @@ export default function App() {
             source={require('./assets/mac.png')}
           />
           <Text style={styles.textDescription}>
-          Embora detalhes específicos sobre o MacBook Pro 2026 ainda não tenham sido oficialmente divulgados pela Apple, espera-se que este modelo apresente avanços significativos em relação às versões anteriores. Rumores indicam que o MacBook Pro 2026 poderá incorporar o chip M6 da Apple, oferecendo desempenho aprimorado e maior eficiência energética. Além disso, há especulações sobre um design mais fino e leve, possivelmente com telas OLED para cores mais vibrantes e pretos profundos. No entanto, devido a atrasos na tecnologia OLED, o lançamento pode ter sido adiado.
+            Embora detalhes específicos sobre o MacBook Pro 2026 ainda não tenham sido oficialmente divulgados pela Apple, espera-se que este modelo apresente avanços significativos em relação às versões anteriores. Rumores indicam que o MacBook Pro 2026 poderá incorporar o chip M6 da Apple, oferecendo desempenho aprimorado e maior eficiência energética. Além disso, há especulações sobre um design mais fino e leve, possivelmente com telas OLED para cores mais vibrantes e pretos profundos. No entanto, devido a atrasos na tecnologia OLED, o lançamento pode ter sido adiado.
           </Text>
+          <TouchableOpacity style={styles.arrowContainer}>
+            <Text style={styles.arrow}>➡️</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.pc}>
@@ -26,8 +29,11 @@ export default function App() {
             source={require('./assets/Dell.png')}
           />
           <Text style={styles.textDescription}>
-          O Dell XPS 13 é conhecido por seu design elegante e desempenho robusto. A versão de 2021 apresentou processadores Intel Core i7 de 11ª geração e placas gráficas integradas Iris Xe, garantindo desempenho sólido para tarefas cotidianas e profissionais. O modelo de 2026 provavelmente continuará essa tendência, incorporando os mais recentes processadores e melhorias em design e funcionalidade. 
+            O Dell XPS 13 é conhecido por seu design elegante e desempenho robusto. A versão de 2021 apresentou processadores Intel Core i7 de 11ª geração e placas gráficas integradas Iris Xe, garantindo desempenho sólido para tarefas cotidianas e profissionais. O modelo de 2026 provavelmente continuará essa tendência, incorporando os mais recentes processadores e melhorias em design e funcionalidade.
           </Text>
+          <TouchableOpacity style={styles.arrowContainer}>
+            <Text style={styles.arrow}>➡️</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.pc}>
@@ -38,6 +44,9 @@ export default function App() {
           <Text style={styles.textDescription}>
             O Razer Blade 15 é um dos laptops para jogos mais sofisticados do mercado. Com uma tela de 15,6 polegadas e opções de resolução 4K, o Razer Blade 15 oferece uma experiência de jogo incrivelmente fluida e visualmente deslumbrante. Equipado com uma placa gráfica NVIDIA RTX 30 Series e processadores Intel Core i9, ele é perfeito para jogos AAA e tarefas de alta demanda como modelagem 3D e renderização de vídeos.
           </Text>
+          <TouchableOpacity style={styles.arrowContainer}>
+            <Text style={styles.arrow}>➡️</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.pc}>
@@ -46,8 +55,11 @@ export default function App() {
             source={require('./assets/alien.png')}
           />
           <Text style={styles.textDescription}>
-          O Alienware Aurora R15 é um desktop gamer da Dell, projetado para oferecer desempenho excepcional em jogos e aplicações intensivas. Embora as especificações possam variar, geralmente inclui processadores Intel Core i9 ou AMD Ryzen de última geração, placas gráficas Nvidia GeForce RTX ou AMD Radeon, e sistemas de resfriamento avançados. O design é futurista, característico da linha Alienware.
+            O Alienware Aurora R15 é um desktop gamer da Dell, projetado para oferecer desempenho excepcional em jogos e aplicações intensivas. Embora as especificações possam variar, geralmente inclui processadores Intel Core i9 ou AMD Ryzen de última geração, placas gráficas Nvidia GeForce RTX ou AMD Radeon, e sistemas de resfriamento avançados. O design é futurista, característico da linha Alienware.
           </Text>
+          <TouchableOpacity style={styles.arrowContainer}>
+            <Text style={styles.arrow}>➡️</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.pc}>
@@ -58,6 +70,9 @@ export default function App() {
           <Text style={styles.textDescription}>
             O MSI GE76 Raider é um laptop de 17,3 polegadas focado em jogos e produtividade, oferecendo uma tela de alta taxa de atualização e uma placa gráfica NVIDIA GeForce RTX 3070. Este laptop é ideal para jogos de alta intensidade e também pode ser usado para criação de conteúdo, como edição de vídeos e design gráfico. Com um processador Intel i9 e até 64 GB de RAM, o GE76 Raider oferece um desempenho impressionante, aliado a um design robusto e moderno.
           </Text>
+          <TouchableOpacity style={styles.arrowContainer}>
+            <Text style={styles.arrow}>➡️</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.pc}>
@@ -68,6 +83,9 @@ export default function App() {
           <Text style={styles.textDescription}>
             O Microsoft Surface Studio 3 é um desktop tudo-em-um voltado para criadores de conteúdo. Sua tela sensível ao toque de 28 polegadas e a capacidade de inclinação de 20 graus torna o Surface Studio 3 perfeito para designers gráficos, editores de vídeo e artistas digitais. Equipado com processadores Intel Core i7 ou i9 e placas gráficas NVIDIA GTX ou RTX, ele oferece excelente desempenho para tarefas criativas intensas e produtividade.
           </Text>
+          <TouchableOpacity style={styles.arrowContainer}>
+            <Text style={styles.arrow}>➡️</Text>
+          </TouchableOpacity>
         </View>
 
       </View>
@@ -78,50 +96,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',  
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'space-evenly',
   },
 
   title: {
-    textShadowColor: "limegreen",  
+    textShadowColor: "limegreen",
     textShadowOffset: { width: 0, height: 5 },
     textShadowRadius: 10,
-    color: "lightgreen",  
-    fontSize: 60,  
+    color: "lightgreen",
+    fontSize: 60,
     fontStyle: "italic",
-    fontWeight: 'bold',  
-    letterSpacing: 2,  
+    fontWeight: 'bold',
+    letterSpacing: 2,
   },
 
   pc: {
-    maxWidth: "45%",  
-    margin: "4%",  
-    shadowColor: 'limegreen',  
+    maxWidth: "45%",
+    margin: "4%",
+    shadowColor: 'limegreen',
     shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.7, 
+    shadowOpacity: 0.7,
     shadowRadius: 25,
-    elevation: 15,  
-    borderRadius: 15, 
-    borderColor: 'limegreen',  
-    borderWidth: 2,  
+    elevation: 15,
+    borderRadius: 15,
+    borderColor: 'limegreen',
+    borderWidth: 2,
   },
 
   textDescription: {
     flexWrap: "wrap",
     width: '100%',
     textAlign: "left",
-    padding: "2%",  
-    color: "lightgreen",  
+    padding: "2%",
+    color: "lightgreen",
     fontStyle: 'italic',
-    fontSize: 18,  
-    lineHeight: 25,  
+    fontSize: 18,
+    lineHeight: 25,
   },
 
   places: {
     width: "100%",
-    borderRadius: 10,  
-    borderWidth: 3, 
-    borderColor: 'limegreen',  
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: 'limegreen',
+  },
+
+  arrowContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  arrow: {
+    fontSize: 40,
+    color: 'limegreen',
   }
 });
